270edcb587ba812be60a7008d0145c9f1ce942cf
Sergei Fedorov <s.fedorov.mhi@yandex.ru>
2024-02-12 22:43:51 +0300

    add mask for coonection to mongoDB

--- example2/main.py
+++ example2/main.py
@@ -3,6 +3,7 @@
 from pymongo import MongoClient
 
 uri = "mongodb+srv://sfedorovmhi:BRL7gP4Dodz4qc3l@testcluster.ykbmvay.mongodb.net/?retryWrites=true&w=majority"
+# mongodb+srv://sfedorovmhi:<password>@testcluster.ykbmvay.mongodb.net/?retryWrites=true&w=majority
 client = MongoClient(uri)
 
 db = client.testdata
